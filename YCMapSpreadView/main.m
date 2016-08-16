66ac9ddee454ec7e6ffdbee8b10824e9f54ad8c1
yangxiaochen <1070438954@qq.com>
2016-08-16 10:16:45 +0800

    init

---
+++ YCMapSpreadView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YCMapSpreadView
+//
+//  Created by tm on 16/8/16.
+//  Copyright © 2016年 tm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
