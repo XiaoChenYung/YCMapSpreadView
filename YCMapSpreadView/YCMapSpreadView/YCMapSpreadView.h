02af57424b785f9ff26cc6335cb3bd8d4dda8271
yangxiaochen <1070438954@qq.com>
2016-08-16 11:11:04 +0800

    添加view

---
+++ YCMapSpreadView/YCMapSpreadView/YCMapSpreadView.h
@@ -0,0 +1,13 @@
+//
+//  YCMapSpreadView.h
+//  YCMapSpreadView
+//
+//  Created by tm on 16/8/16.
+//  Copyright © 2016年 tm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YCMapSpreadView : UIView
+
+@end
---
+++ YCMapSpreadView/YCMapSpreadView/YCMapSpreadView.m
@@ -0,0 +1,32 @@
+//
+//  YCMapSpreadView.m
+//  YCMapSpreadView
+//
+//  Created by tm on 16/8/16.
+//  Copyright © 2016年 tm. All rights reserved.
+//
+
+#import "YCMapSpreadView.h"
+
+@implementation YCMapSpreadView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+//- (instancetype)mj_refreshBundle
+//{
+//    static NSBundle *refreshBundle = nil;
+//    if (refreshBundle == nil) {
+//        // 这里不使用mainBundle是为了适配pod 1.x和0.x
+//        refreshBundle = [NSBundle bundleWithPath:[[NSBundle bundleForClass:[YCMapSpreadView class]] pathForResource:@"MJRefresh" ofType:@"bundle"]];
+//    }
+//    return refreshBundle;
+//}
+
+@end
